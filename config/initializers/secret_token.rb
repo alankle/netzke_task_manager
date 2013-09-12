f0356257ce4a90c747bd389162aad01c99b01e93
Alan Kleber <alankle@gmail.com>
2013-09-12 09:34:03 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeTaskManager::Application.config.secret_token = '7385d1a71be43abfc94ead72edd37bf8a0c04a505404b62b699dec3f35961ec4b73e063cc9d7ebe314ef11f3a9fbbb43254ee69e0179005ab11001b8c85b3aef'
